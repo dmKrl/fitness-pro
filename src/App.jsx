f888510effd1302634e86863d694b9153b7b3dd5
Dmitry Kralichkin <dm_krl@mail.ru>
2024-01-15 01:05:34 +1200

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,40 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { setDataCourses } from './redux/slices/dataSlices';
 import GlobalStyle from './GlobalStyle.styles';
 import AppRoutes from './routes';
 import { db } from './firebase-config';
+import { setDataCourses } from './redux/slices/dataSlices';
+import data from './data.json';
 
 function App() {
     const dispatch = useDispatch();
+    const addTodo = async () => {
+        try {
+            const docRef = await addDoc(collection(db, 'data'), {
+                data,
+            });
+            console.log('Document written with ID: ', docRef.id);
+        } catch (error) {
+            console.error('Error adding document: ', error);
+        }
+    };
 
     const coursesCollection = collection(db, 'data');
 
-    // const addTodo = async () => {
-    //     try {
-    //         const docRef = await addDoc(collection(db, 'users'));
-    //         console.log('Document written with ID: ', docRef.id);
-    //     } catch (error) {
-    //         console.error('Error adding document: ', error);
-    //     }
-    // };
-
     const fetchDataCourses = async () => {
-        await getDocs(coursesCollection).then((querySnapshot) => {
-            // console.log(querySnapshot);
-            const newData = querySnapshot.docs.map((doc) => ({
-                ...doc.data([]),
-                id: doc.id,
+        await getDocs(coursesCollection).then((arrayCoursesFitting) => {
+            const newData = arrayCoursesFitting.docs.map((doc) => ({
+                ...doc.data(),
             }));
-            // console.log(newData[0]?.dataCourses);
-            dispatch(setDataCourses(newData[0]?.data));
+            console.log(newData[0]);
+            dispatch(setDataCourses(newData[0]));
         });
     };
     useEffect(() => {
-        // addTodo();
-        setTimeout(() => {
-            fetchDataCourses();
-        }, 0);
+        addTodo();
+        fetchDataCourses();
     }, []);
-
-    // console.log(courses);
-
     return (
         <>
             <GlobalStyle />
