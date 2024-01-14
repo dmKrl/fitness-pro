8d5e58c430fbdc2d70b72481ff5446204fd0c85a
Dmitry Kralichkin <dm_krl@mail.ru>
2024-01-15 00:30:25 +1200

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,45 @@
 import { collection, getDocs } from 'firebase/firestore';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { setDataCourses } from './redux/slices/dataSlices';
 import GlobalStyle from './GlobalStyle.styles';
 import AppRoutes from './routes';
 import { db } from './firebase-config';
-import { setDataCourses } from './redux/slices/dataSlices';
 
 function App() {
     const dispatch = useDispatch();
+
+    const coursesCollection = collection(db, 'data');
+
     // const addTodo = async () => {
     //     try {
-    //         const docRef = await addDoc(collection(db, 'data'), {
-    //             data,
-    //         });
+    //         const docRef = await addDoc(collection(db, 'users'));
     //         console.log('Document written with ID: ', docRef.id);
     //     } catch (error) {
     //         console.error('Error adding document: ', error);
     //     }
     // };
 
-    const coursesCollection = collection(db, 'data');
-
     const fetchDataCourses = async () => {
-        await getDocs(coursesCollection).then((arrayCoursesFitting) => {
-            const newData = arrayCoursesFitting.docs.map((doc) => ({
-                ...doc.data(),
+        await getDocs(coursesCollection).then((querySnapshot) => {
+            // console.log(querySnapshot);
+            const newData = querySnapshot.docs.map((doc) => ({
+                ...doc.data([]),
+                id: doc.id,
             }));
-            console.log(newData[0]);
-            dispatch(setDataCourses(newData[0]));
+            // console.log(newData[0]?.dataCourses);
+            dispatch(setDataCourses(newData[0]?.data));
         });
     };
     useEffect(() => {
-        fetchDataCourses();
+        // addTodo();
+        setTimeout(() => {
+            fetchDataCourses();
+        }, 0);
     }, []);
+
+    // console.log(courses);
+
     return (
         <>
             <GlobalStyle />
